f485d3e3a10a8638940937ad0103228320130645
sasha <sjlarson92@gmail.com>
2020-04-02 14:18:10 -0500

    changed db environment var to be one profile for all schemas

---
+++ src/main/java/com/dap/DailyArtPrompt/configuration/DataSourceConfig.java
@@ -0,0 +1,29 @@
+package com.dap.DailyArtPrompt.configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        System.out.println("dbUL: " + dbUrl);
+        System.out.println("username: " + username);
+        System.out.println("password: " + password);
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
