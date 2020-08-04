194d7d6f2953452643ce699a6254328cf55c4f9a
sasha <sjlarson92@gmail.com>
2020-08-03 22:06:01 -0500

    added endpoint to get raw image

--- src/main/java/com/dap/DailyArtPrompt/controller/ImageController.java
+++ src/main/java/com/dap/DailyArtPrompt/controller/ImageController.java
@@ -1,15 +1,25 @@
 package com.dap.DailyArtPrompt.controller;
 
 import com.dap.DailyArtPrompt.entity.Image;
+import com.dap.DailyArtPrompt.factory.AWSObjectsFactory;
 import com.dap.DailyArtPrompt.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.view.RedirectView;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
+import java.time.Duration;
 import java.util.UUID;
 
 @RestController
@@ -19,6 +29,17 @@ public class ImageController {
 
     private final ImageRepository imageRepository;
 
+    private final AWSObjectsFactory awsObjectsFactory;
+
+    @Value("${S3_BUCKET}")
+    String s3Bucket;
+
+    @Value("${ACCESS_KEY}")
+    String accessKey;
+
+    @Value("${SECRET_KEY}")
+    String secretKey;
+
     @GetMapping("/images/{id}")
     public Image getImage(@PathVariable UUID id) {
         log.info("Fetching image with id " + id);
@@ -26,35 +47,30 @@ public class ImageController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No event found by given id: " + id));
     }
 
-//    @GetMapping("/images/{id}/content")
-//    public RedirectView getImageFromS3Bucket(@PathVariable UUID id) {
-//        log.info("Fetching image with name: " + id);
-//
-//        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-//                .bucket("daily-art-prompt-vmkugdgwiz")
-//                .key("dap/images/" + id)
-//                .build();
-//
-//        // Create a GetObjectPresignRequest to specify the signature duration
-//        GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
-//                .signatureDuration(Duration.ofMinutes(10))
-//                .getObjectRequest(getObjectRequest)
-//                .build();
-//
-//        ProfileCredentialsProvider pcp = ProfileCredentialsProvider.builder()
-//                .build();
-//
-//        S3Presigner s3Presigner = S3Presigner.builder()
-//                .credentialsProvider(pcp)
-//                .region(Region.US_EAST_2)
-//                .build();
-//
-//        // Generate the presigned request
-//        PresignedGetObjectRequest presignedGetObjectRequest = s3Presigner.presignGetObject(getObjectPresignRequest);
-//
-//        s3Presigner.close();
-//
-//        String url = presignedGetObjectRequest.url().toString();
-//        return new RedirectView(url);
-//    }
+    @GetMapping("/images/{id}/content")
+    public RedirectView getImageFromS3Bucket(@PathVariable UUID id) {
+        log.info("Fetching image with id: " + id);
+
+        GetObjectRequest getObjectRequest = awsObjectsFactory.createGetObjectRequest(
+                s3Bucket, "dap/images/" + id
+        );
+
+        // Create a GetObjectPresignRequest to specify the signature duration
+        GetObjectPresignRequest getObjectPresignRequest = awsObjectsFactory.createGetObjectPresignRequest(
+                Duration.ofMinutes(10),
+                getObjectRequest
+        );
+
+        AwsCredentialsProvider awsCredentialsProvider = awsObjectsFactory.createAwsCredentialsProvider(
+                accessKey, secretKey);
+
+        S3Presigner s3Presigner = awsObjectsFactory.createS3Presigner(awsCredentialsProvider, Region.US_EAST_2);
+
+        // Generate the presigned request
+        PresignedGetObjectRequest presignedGetObjectRequest = s3Presigner
+                .presignGetObject(getObjectPresignRequest);
+
+        s3Presigner.close();
+        return new RedirectView(presignedGetObjectRequest.url().toString());
+    }
 }
--- src/test/java/com/dap/DailyArtPrompt/controller/ImageControllerTest.java
+++ src/test/java/com/dap/DailyArtPrompt/controller/ImageControllerTest.java
@@ -1,8 +1,10 @@
 package com.dap.DailyArtPrompt.controller;
 
 import com.dap.DailyArtPrompt.entity.Image;
+import com.dap.DailyArtPrompt.factory.AWSObjectsFactory;
 import com.dap.DailyArtPrompt.repository.ImageRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -12,20 +14,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
+import java.time.Duration;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(ImageController.class)
+@WebMvcTest(
+        value = ImageController.class,
+        properties = {
+                "S3_BUCKET=some-mock-s3-bucket",
+                "ACCESS_KEY=some key",
+                "SECRET_KEY=super secret key"
+        }
+)
 @ExtendWith(MockitoExtension.class)
 class ImageControllerTest {
+
+    String s3Bucket = "some-mock-s3-bucket"; // same as properties above
+    String accessKey = "some key";
+    String secretKey = "super secret key";
 
     @Autowired
     private MockMvc mockMvc;
@@ -35,6 +56,9 @@ class ImageControllerTest {
 
     @MockBean
     ImageRepository imageRepository;
+
+    @MockBean
+    AWSObjectsFactory awsObjectsFactory;
 
     @Nested
     @DisplayName("/images")
@@ -64,10 +88,91 @@ class ImageControllerTest {
                 String message = Objects.requireNonNull(
                         mockMvc.perform(get("/images/" + imageId))
                                 .andExpect(status().isNotFound())
-                        .andReturn().getResolvedException()).getMessage();
+                                .andReturn().getResolvedException()).getMessage();
 
                 assertThat(message).contains("No event found by given id: " + imageId);
             }
+        }
+    }
+
+    @Nested
+    class getImageFromS3Bucket {
+
+        private final UUID id = UUID.randomUUID();
+        private final GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("randomBucket")
+                .key("some key")
+                .build();
+        private final GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .getObjectRequest(getObjectRequest)
+                .build();
+        private final AwsCredentialsProvider awsCredentialsProvider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey));
+        private final S3Presigner s3Presigner = S3Presigner.builder()
+                .credentialsProvider(awsCredentialsProvider)
+                .region(Region.US_EAST_2)
+                .build();
+
+        @BeforeEach
+        public void setMocks() {
+            reset(awsObjectsFactory);
+            when(awsObjectsFactory.createGetObjectRequest(
+                    s3Bucket,
+                    "dap/images/" + id
+                    )
+            ).thenReturn(getObjectRequest);
+
+            when(awsObjectsFactory.createGetObjectPresignRequest(
+                    Duration.ofMinutes(10),
+                    getObjectRequest
+            )).thenReturn(getObjectPresignRequest);
+
+            when(awsObjectsFactory.createAwsCredentialsProvider(
+                    accessKey,
+                    secretKey
+            )).thenReturn(awsCredentialsProvider);
+
+            when(awsObjectsFactory.createS3Presigner(
+                    awsCredentialsProvider,
+                    Region.US_EAST_2
+            )).thenReturn(s3Presigner);
+        }
+
+        @Test
+        public void createsAGetObjectRequestWithTheCorrectParams() throws Exception {
+            mockMvc.perform(get("/images/" + id + "/content"));
+            verify(awsObjectsFactory).createGetObjectRequest(
+                    s3Bucket,
+                    "dap/images/" + id
+            );
+        }
+
+        @Test
+        public void createsAGetObjectPresignRequestWithCorrectParams() throws Exception {
+            mockMvc.perform(get("/images/" + id + "/content"));
+            verify(awsObjectsFactory).createGetObjectPresignRequest(
+                    Duration.ofMinutes(10),
+                    getObjectRequest
+            );
+        }
+
+        @Test
+        public void createAwsCredentialProviderWithCorrectParams() throws Exception {
+            mockMvc.perform(get("/images/" + id + "/content"));
+            verify(awsObjectsFactory).createAwsCredentialsProvider(
+                    accessKey,
+                    secretKey
+            );
+        }
+
+        @Test
+        public void createS3PresignerWithCorrectParams() throws Exception {
+            mockMvc.perform(get("/images/" + id + "/content"));
+            verify(awsObjectsFactory).createS3Presigner(
+                    awsCredentialsProvider,
+                    Region.US_EAST_2
+            );
         }
     }
 }
