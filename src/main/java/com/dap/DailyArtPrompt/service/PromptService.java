5b080b3515b64621b0402dd30e10b720a196c1bf
sasha <sjlarson92@gmail.com>
2020-11-07 12:43:06 -0600

    add prompt POST to generate prompts, update Prompt fields,add api call to get prompt words, add db changes for prompt table

--- src/main/java/com/dap/DailyArtPrompt/service/PromptService.java
+++ src/main/java/com/dap/DailyArtPrompt/service/PromptService.java
@@ -3,29 +3,54 @@ package com.dap.DailyArtPrompt.service;
 import com.dap.DailyArtPrompt.entity.Prompt;
 import com.dap.DailyArtPrompt.repository.PromptRepository;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class PromptService {
+
     private final PromptRepository promptRepository;
+    private final RestTemplate restTemplate;
 
-    public PromptService(PromptRepository promptRepository) {
-        this.promptRepository = promptRepository;
-    }
+//    public Map<LocalDate, Prompt> getAllPrompts() {
+//        List<Prompt> allPromptsList = promptRepository.findAll();
+//
+//        Map<LocalDate, Prompt> allPromptsMap = new HashMap<>();
+//
+//        for (Prompt prompt : allPromptsList) {
+//            LocalDate date = LocalDate.parse(prompt.getDate());
+//            allPromptsMap.put(date, prompt);
+//        }
+//
+//        return allPromptsMap;
+//    }
 
-    public Map<LocalDate, Prompt> getAllPrompts() {
-        List<Prompt> allPromptsList = promptRepository.findAll();
-
-        Map<LocalDate, Prompt> allPromptsMap = new HashMap<>();
-
-        for (Prompt prompt : allPromptsList) {
-            LocalDate date = LocalDate.parse(prompt.getDate());
-            allPromptsMap.put(date, prompt);
-        }
-
-        return allPromptsMap;
+    public void createPrompts() {
+        String response = restTemplate.exchange(
+                "https://writingexercises.co.uk/php/nouns.php",
+                HttpMethod.GET,
+                null,
+                String.class).getBody();
+        System.out.println("response: " + response);
+        List<String> words = Arrays.asList(response.split(" "));
+        // TODO: get most recent date from db and then add to that
+        LocalDate date = LocalDate.now();
+        words.forEach(word -> {
+            System.out.println("word: " + word);
+            System.out.println("date: " + date);
+            Prompt newPrompt = new Prompt();
+            newPrompt.setId(UUID.randomUUID());
+            newPrompt.setDate(date);
+            newPrompt.setText(word);
+            promptRepository.save(newPrompt);
+            date.plusDays(1L);
+        });
     }
 }
