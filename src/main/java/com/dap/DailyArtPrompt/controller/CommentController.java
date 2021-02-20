41cf333cd858b9f7a2491349a29bb5f37f4d5a98
sasha <sjlarson92@gmail.com>
2021-02-20 14:55:30 -0600

    add log

--- src/main/java/com/dap/DailyArtPrompt/controller/CommentController.java
+++ src/main/java/com/dap/DailyArtPrompt/controller/CommentController.java
@@ -4,18 +4,21 @@ import com.dap.DailyArtPrompt.entity.Comment;
 import com.dap.DailyArtPrompt.model.CommentRequestBody;
 import com.dap.DailyArtPrompt.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CommentController {
 
     private final CommentService commentService;
 
     @PostMapping("/comments")
     public Comment createComment(@Valid @RequestBody CommentRequestBody commentRequestBody) {
+        log.info("Saving comment with the following requestBody: " + commentRequestBody);
         return commentService.createComment(commentRequestBody);
     }
 
