5230366113316784e607e52a16c4a439c1d73856
sasha <sjlarson92@gmail.com>
2021-02-20 19:28:36 -0600

    add getComments endpoint

--- src/main/java/com/dap/DailyArtPrompt/service/CommentService.java
+++ src/main/java/com/dap/DailyArtPrompt/service/CommentService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -24,5 +25,9 @@ public class CommentService {
                 OffsetDateTime.now()
         );
         return commentRepository.save(comment);
+    }
+
+    public List<Comment> getComments(UUID imageId) {
+        return commentRepository.getCommentsByImageId(imageId);
     }
 }
