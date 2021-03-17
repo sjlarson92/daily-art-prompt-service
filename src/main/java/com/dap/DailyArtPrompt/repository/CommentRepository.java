65b6c9fb3eb6b52e647566b4a9d7e848cf73e2a2
sasha <sjlarson92@gmail.com>
2021-03-16 20:08:08 -0500

    add delete image endpoint and delete associated comments

--- src/main/java/com/dap/DailyArtPrompt/repository/CommentRepository.java
+++ src/main/java/com/dap/DailyArtPrompt/repository/CommentRepository.java
@@ -4,10 +4,14 @@ import com.dap.DailyArtPrompt.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
     List<Comment> getCommentsByImageId(UUID imageId);
+
+    @Transactional
+    void deleteAllByImageId(UUID imageId);
 }
--- src/main/java/com/dap/DailyArtPrompt/service/ImageService.java
+++ src/main/java/com/dap/DailyArtPrompt/service/ImageService.java
@@ -1,6 +1,7 @@
 package com.dap.DailyArtPrompt.service;
 
 import com.dap.DailyArtPrompt.entity.Image;
+import com.dap.DailyArtPrompt.repository.CommentRepository;
 import com.dap.DailyArtPrompt.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,8 @@ public class ImageService {
 
     private final ImageRepository imageRepository;
 
+    private final CommentRepository commentRepository;
+
     public Image getImage(UUID id) {
         return imageRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No event found by given id: " + id));
@@ -32,5 +35,10 @@ public class ImageService {
 
     public List<Image> getCommunityImagesByPromptIdAndUserId(UUID promptId, UUID userId) {
         return imageRepository.findAllByPromptIdAndUserIdNot(promptId, userId);
+    }
+
+    public void deleteImage(UUID id) {
+        commentRepository.deleteAllByImageId(id);
+        imageRepository.deleteById(id);
     }
 }
